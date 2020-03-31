8b7db103a9c192c3d3f10fb537e8400d15e3ac11
ylp39 <747136311@qq.com>
2020-03-31 17:28:15 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/ylp/coolweather/gson/Now.java
+++ app/src/main/java/com/ylp/coolweather/gson/Now.java
@@ -5,12 +5,12 @@ import com.google.gson.annotations.SerializedName;
 public class Now {
 
     @SerializedName("tmp")
-    public String tmp;
+    public String temperature;
 
     @SerializedName("cond")
-    public Cond cond;
+    public More more;
 
-    public class Cond{
+    public class More{
         @SerializedName("txt")
         public String info;
     }
